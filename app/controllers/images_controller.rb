1684ac26efb35dfd0f7187da7808e64d75e23e21
Gwen <wenqingou@gmail.com>
2016-12-22 20:07:24 +0000

    install bootstrap

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,55 @@
+class ImagesController < ApplicationController
+before_action :set_image, only:[:update, :show, :edit, :destroy] 
+
+  def index
+    @images = Image.all
+  
+  end 
+  
+  def new
+    @image = Image.new
+  end 
+  
+  def create
+    @image = Image.new(image_params)
+    if @image.save
+      flash[:notice]="Your image has been successfully created"
+      redirect_to image_path(@image)
+    else
+      render 'new'
+    end 
+  end
+  
+
+  
+  def edit
+  end 
+  
+  def update
+    if @image.update(image_params)
+      flash[:notice]="Your image has been successfully updated"
+      redirect_to image_path(@image)
+    else
+      render 'edit'
+    end
+  end
+  
+  def show
+  end 
+  
+  def destroy
+    @image.destroy
+    flash[:notice]="Your image has been successfully deleted"
+    redirect_to images_path
+  end 
+
+  private
+    def image_params
+      params.require(:image).permit(:imagename, :category) 
+    end 
+    def set_image
+      @image = Image.find(params[:id])
+    end 
+    
+  
+end 
