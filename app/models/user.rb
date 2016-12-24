9da8becbd1a37c9eacfa04f7be3348a6b2aca849
Gwen <wenqingou@gmail.com>
2016-12-24 08:22:13 +0000

    Style homepage using bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :images
-  before_save{self.email = email.downcase}
+  before_save {self.email = email.downcase}
   validates :username, presence: true, 
                         uniqueness: {case_sensitive:false}, 
                         length: {minimum:3, maximum:25}
@@ -12,5 +12,4 @@ class User < ActiveRecord::Base
                     format: {with: VALID_EMAIL_REGEX}
                     
   has_secure_password
-  
 end 
