69ebd9502f477a0de80cd61e22a596e2e0a8ec8e
Gwen <wenqingou@gmail.com>
2016-12-25 00:55:18 +0000

    make login form, style profile pages and filter page

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ resources :images
 get 'signup', to: "users#new"
 
 resources :users, except: [:new]
+get 'login', to: "sessions#new"
+post 'login', to: "sessions#create"
+delete 'logout', to: "sessions#destroy"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
